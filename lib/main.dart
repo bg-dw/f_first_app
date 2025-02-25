5f1fb1c3b872650a728e0caf43836955432e2b75
bg-dw <bg.dwik@gmail.com>
2025-02-25 15:30:52 +0700

    Belajar styling background dan text

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,19 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      body: Center(
-        child: Text("hello world!"),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(colors: [
+            Color.fromARGB(255, 10, 105, 213),
+            Color.fromARGB(255, 115, 171, 246)
+          ], begin: Alignment.bottomCenter, end: Alignment.topCenter),
+        ),
+        child: const Center(
+          child: Text(
+            "hello world!",
+            style: TextStyle(color: Colors.white, fontSize: 28),
+          ),
+        ),
       ),
     ),
   ));
